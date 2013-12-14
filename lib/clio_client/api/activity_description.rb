cdde150e1bf634b41df961ebf8979e6600ee591f
Kyle d'Oliveira <kyle@goclio.com>
2013-12-14 11:19:40 -0800

    Actually adding the ad endpoint and setting up paging

---
+++ lib/clio_client/api/activity_description.rb
@@ -0,0 +1,17 @@
+module ClioClient
+  module Api
+    class ActivityDescription < Base
+
+      include ClioClient::Api::Listable
+
+      private
+      def data_klass(*args)
+        ClioClient::ActivityDescription
+      end
+
+      def end_point_url; 'activity_descriptions'; end
+      def plural_resource; 'activity_descriptions'; end
+
+    end
+  end
+end
--- lib/clio_client/api/listable.rb
+++ lib/clio_client/api/listable.rb
@@ -3,9 +3,27 @@ module ClioClient
 
     module Listable
       def list(params = {})
+        @last_query = params
+        @records = 0
         response = session.get(end_point_url, params)
+        @next_offset = response["next_offset"]
+        @records += response["records"]
+        @total_records = response["total_records"]
         response[plural_resource].collect{ |r| data_item(r) }
       end
+
+      def more_pages?
+        @records < @total_records
+      end
+      
+      def next_page
+        params = @last_query.merge(:offset => @next_offset)
+        response = session.get(end_point_url, params)
+        @next_offset = response["next_offset"]
+        @records += response["records"]
+        response[plural_resource].collect{ |r| data_item(r) }        
+      end
+
     end
 
   end
