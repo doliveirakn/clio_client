ae92885a5c6809c7f3aed70781403d72ab5974b9
Hanson Wu <hanson.wu@goclio.com>
2014-08-15 13:05:49 -0700

    client_connect_permission_endpoint CR comments

--- lib/clio_client/api/crudable.rb
+++ lib/clio_client/api/crudable.rb
@@ -10,6 +10,17 @@ module ClioClient
         resource = params.is_a?(Array) ? create_plural(params) : create_singular(params)
       end
 
+      def update(id, params = {})
+        response = session.put("#{end_point_url}/#{id}", {singular_resource => params}.to_json)
+        data_item(response[singular_resource])        
+      end
+
+      def destroy(id)
+        session.delete("#{end_point_url}/#{id}", false)
+      end
+
+      private
+
       def create_singular(params)
         response = session.post(end_point_url, {singular_resource => params}.to_json)
         data_item(response[singular_resource])
@@ -18,15 +29,6 @@ module ClioClient
       def create_plural(params)
         response = session.post(end_point_url, {plural_resource => params}.to_json)
         response[plural_resource].map { |resource| data_item(resource) }
-      end
-
-      def update(id, params = {})
-        response = session.put("#{end_point_url}/#{id}", {singular_resource => params}.to_json)
-        data_item(response[singular_resource])        
-      end
-
-      def destroy(id)
-        session.delete("#{end_point_url}/#{id}", false)
       end
 
     end
--- lib/clio_client/models/client_connect_permission.rb
+++ lib/clio_client/models/client_connect_permission.rb
@@ -1,18 +1,17 @@
 module ClioClient
   class ClientConnectPermission < Resource
 
-    set_attributes(id:            {type: :int, readonly: true},
-                   created_at:    {type: :datetime, readonly: true},
-                   updated_at:    {type: :datetime, readonly: true},
-                   user_id:       {type: :int, readonly: true},
-                   matter_id:     {type: :int},
-                   contact_id:    {type: :int},
-                   resource_type: {type: :string},
-                   resource_id:   {type: :int}
+    set_attributes(id:                     {type: :int, readonly: true},
+                   created_at:             {type: :datetime, readonly: true},
+                   updated_at:             {type: :datetime, readonly: true},
+                   client_connect_user_id: {type: :int, readonly: true},
+                   resource_type:          {type: :string},
+                   resource_id:            {type: :int}
                    )
 
     has_association :contact, ClioClient::Contact
     has_association :user,    ClioClient::User
+    has_association :matter,  ClioClient::Matter
 
     private
 
