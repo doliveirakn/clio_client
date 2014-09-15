206aa16234242babfb1d0b9a337fae9b85bd363c
Marc Barlo <marc.barlo@goclio.com>
2014-09-15 16:58:32 -0700

    add document version note to models list

--- lib/clio_client/support/document_version_note.rb
+++ lib/clio_client/support/document_version_note.rb
@@ -1,6 +1,6 @@
 module ClioClient
   class DocumentVersionNote < Resource
-    
+
     set_attributes(user_id:                 {type: :int, readonly: true},
                    subject:                 {type: :string},
                    detail:                  {type: :string}, 
@@ -9,5 +9,4 @@ module ClioClient
                    )
 
   end
-
 end
