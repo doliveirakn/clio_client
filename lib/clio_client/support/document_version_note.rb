7bada289ac77990551ed2094834756252e9a5489
Marc Barlo <marc.barlo@goclio.com>
2014-09-15 16:49:17 -0700

    add document version note resource and attach to the document version model

---
+++ lib/clio_client/support/document_version_note.rb
@@ -0,0 +1,13 @@
+module ClioClient
+  class DocumentVersionNote < Resource
+    
+    set_attributes(user_id:                 {type: :int, readonly: true},
+                   subject:                 {type: :string},
+                   detail:                  {type: :string}, 
+                   created_at:              {type: :datetime, readonly: true},
+                   updated_at:              {type: :datetime, readonly: true},
+                   )
+
+  end
+
+end
